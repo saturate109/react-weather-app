89454df21feb874a2abe134b1849d83e720d95de
saturate109 <56941451+saturate109@users.noreply.github.com>
2021-03-13 22:34:12 +0000

    Add Weather component to index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Weather from 'layouts/Weather';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Weather />
   </React.StrictMode>,
   document.getElementById('root')
 );
